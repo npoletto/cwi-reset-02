308910f397fc342af56e87edad8c3bd8f492f8fd
Nicolai Poletto <nicolaipoletto@yahoo.com.br>
2021-02-13 17:43:49 -0300

    IMplementacao de condições na avaliacao

--- Exercicios-aula02/src/exercicio01/Filme.java
+++ Exercicios-aula02/src/exercicio01/Filme.java
@@ -10,14 +10,21 @@ public class Filme {
 
     public Filme(String nome, Diretor diretor, String descricao, int ano, int duracao, int avaliacao) {
         this.nome = nome;
+
         this.diretor = diretor;
         this.descricao = descricao;
         this.ano = ano;
         this.duracao = duracao;
-        if(avaliacao<0 || avaliacao>5) {
-            throw new IllegalArgumentException("A avaliação precisa ser entre 0 e 5.");
+        if(nome.equalsIgnoreCase("o clube da luta")) {
+            avaliacao = 5;
+        } else if(nome.equalsIgnoreCase("batman vs superman")) {
+            avaliacao = 1;
+        } else if(avaliacao<1 || avaliacao>5) {
+            avaliacao = 3;
+            //throw new IllegalArgumentException("A avaliação precisa ser entre 0 e 5.");
+        } else {
+            this.avaliacao = avaliacao;
         }
-        this.avaliacao = avaliacao;
 
     }
 
