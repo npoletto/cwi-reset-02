de6e51be92329f126aa316558c405185e06d7aef
Nicolai Poletto <nicolaipoletto@yahoo.com.br>
2021-02-13 11:28:32 -0300

    Alteração na descrição do filme

--- Exercicios/src/aula02/exercicio1/Aplicacao.java
+++ Exercicios/src/aula02/exercicio1/Aplicacao.java
@@ -3,7 +3,11 @@ package aula02.exercicio1;
 public class Aplicacao {
     public static void main(String[] args) {
         Diretor coppola = new Diretor("Francis Ford Coppola", 81, 27);
-        Filme godfather = new Filme("The Godfather", coppola, "Filme sobre máfia italiana", 1972, 5);
+        Filme godfather = new Filme("The Godfather",
+                coppola,
+                " filme norte-americano de 1972, dirigido por Francis Ford Coppola, baseado no livro homônimo escrito por Mario Puzo. É estrelado por Marlon Brando, Al Pacino, James Caan, Richard Castellano, Robert Duvall, Sterling Hayden, John Marley, Richard Conte e Diane Keaton. O enredo se baseia na história da família mafiosa Corleone, de 1945 até 1955. Teve duas sequências: The Godfather: Part II, em 1974; e The Godfather: Part III em 1990.",
+                1972,
+                5);
         Diretor lynch = new Diretor("David Lynch", 75, 14);
         Filme blueVelvet = new Filme("Blue Velvet",
                 lynch,
