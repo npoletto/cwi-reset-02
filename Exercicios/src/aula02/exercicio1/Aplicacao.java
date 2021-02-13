96b9258edde659fc6541ed891422f40fc893a2ab
Nicolai Poletto <nicolaipoletto@yahoo.com.br>
2021-02-13 11:21:59 -0300

    Adição do exercício 1 da aula 2

--- Exercicios/src/aula02/exercicio1/Aplicacao.java
+++ Exercicios/src/aula02/exercicio1/Aplicacao.java
@@ -2,7 +2,20 @@ package aula02.exercicio1;
 
 public class Aplicacao {
     public static void main(String[] args) {
-        Funcionario funcionario = new Funcionario("00000", "Nicolai", 38, 3, "programador", "contratado");
+        Diretor coppola = new Diretor("Francis Ford Coppola", 81, 27);
+        Filme godfather = new Filme("The Godfather", coppola, "Filme sobre máfia italiana", 1972, 5);
+        Diretor lynch = new Diretor("David Lynch", 75, 14);
+        Filme blueVelvet = new Filme("Blue Velvet",
+                lynch,
+                "filme de longa-metragem de 1986, do gênero suspense, com roteiro e direção de David Lynch e trilha sonora criada por Angelo Badalamenti. O filme é um exemplo de neo-noir e contém elementos de surrealismo.[3]",
+                1986,
+                4);
+
+        System.out.println(godfather);
+        System.out.println("\n");
+        System.out.println(blueVelvet);
 
     }
+
+
 }
---
+++ Exercicios/src/aula02/exercicio1/Diretor.java
@@ -0,0 +1,38 @@
+package aula02.exercicio1;
+
+public class Diretor {
+    private String nome;
+    private int idade;
+    private int qtdFilmesProduzidos;
+
+    public Diretor(String nome, int idade, int qtdFilmesProduzidos) {
+        this.nome = nome;
+        this.idade = idade;
+        this.qtdFilmesProduzidos = qtdFilmesProduzidos;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getQtdFilmesProduzidos() {
+        return qtdFilmesProduzidos;
+    }
+
+    public void setQtdFilmesProduzidos(int qtdFilmesProduzidos) {
+        this.qtdFilmesProduzidos = qtdFilmesProduzidos;
+    }
+
+    public String toString() {
+        return nome + " - idade: " + idade + " - Qtd filmes produzidos: "+ qtdFilmesProduzidos;
+    }
+}
