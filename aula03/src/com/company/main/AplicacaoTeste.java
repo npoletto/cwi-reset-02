21b741c14ee67130d705f4b4c33502a601115663
Nicolai Poletto <nicolaipoletto@yahoo.com.br>
2021-02-20 16:44:51 -0300

    Adição do filme para verificação.

--- aula03/src/com/company/main/AplicacaoTeste.java
+++ aula03/src/com/company/main/AplicacaoTeste.java
@@ -1,11 +1,20 @@
 package com.company.main;
 
+import com.company.domain.Diretor;
 import com.company.domain.Editora;
+import com.company.domain.Filme;
+import com.company.enums.Genero;
 
 public class AplicacaoTeste {
     public static void main(String[] args) {
         try {
-            Editora editora = new Editora("DC Comics", "EUA");
+            Editora editora = new Editora("Marvel", "EUA");
+            Filme filme = new Filme("Cidade de Deus",
+                                    "Melhor filme nacional",
+                                    180,
+                                    2010,
+                                    5,
+                                    new Diretor("padilha", 56, 23, Genero.MASCULINO));
         } catch(Exception ex) {
             System.out.println("Opa, tivemos um problema:\n"+ex.toString());
         }
